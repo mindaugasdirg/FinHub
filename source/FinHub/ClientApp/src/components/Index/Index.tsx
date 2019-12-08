7d0d37e29771f5b84a3180d9b4a14cdd2d82a0e4
Mindaugas <mindaugasdirg@gmail.com>
2019-12-08 14:14:39 +0200

    Add initial index page

---
+++ source/FinHub/ClientApp/src/components/Index/Index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const Index = () => (
+    <Grid container spacing={3}>
+        <Grid item md={6} xs={12}>
+            <Paper>
+                <Typography variant="h1">FinHub</Typography>
+                <Typography variant="h4">Track Group expenses easily!</Typography>
+            </Paper>
+        </Grid>
+        <Grid item md={6} xs={12}>
+            <Paper>
+                <Button variant="contained" color="primary">Create your group</Button>
+                <Button variant="contained" color="primary">Join group</Button>
+                <Typography variant="h6">Already a member?</Typography>
+                <Button>Login</Button>
+            </Paper>
+        </Grid>
+    </Grid>
+);
+
+export default Index;
--- source/FinHub/ClientApp/src/routes/Home.tsx
+++ source/FinHub/ClientApp/src/routes/Home.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Index from '../components/Index/Index';
 
 const Home = () => (
-    <>
-        <div>Home directory</div>
-    </>
+    <Index />
 );
 
 export default Home;
